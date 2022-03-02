8f117ceb43fca19219594bf94a41a62f2c6e0fd8
smo92 <moran.s215@gmail.com>
2022-03-02 12:37:28 -0700

    Creating intial movie API

--- index.js
+++ index.js
@@ -1,53 +1,144 @@
 
+const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan');
+const uuid = require('uuid');
+
 const app = express();
 
-const morgan = require('morgan'),
-    bodyParser = require('body-parser');
-
-app.use(morgan('common'));
 app.use(bodyParser.json());
+app.use(morgan('common'));
 
-let topMovies = [
-    {
-      title: 'Harry Potter and the Sorcerer\'s Stone',
-      author: 'J.K. Rowling'
+let users = [];
+
+let movies = [
+  {
+    title: ' Saving Praiate Ryan',
+    year: '2000',
+    genre:{
+      type:'Action',
+      description: 'Violence and drama, grab your popcorn!'
     },
-    {
-      title: 'Lord of the Rings',
-      author: 'J.R.R. Tolkien'
-    },
-    {
-      title: 'Twilight',
-      author: 'Stephanie Meyer'
+    director:{
+      name:'Steve',
+      birthYear: '1969',
+      deathYear: 'Still alive',
+      bio:'Great guy'
     }
-  ];
+  },
+  {
+    title: 'Gladiator',
+    year: '2000',
+    genre:{
+      type:'Action',
+      description: 'Violence and drama, grab your popcorn!'
+    },
+    director:{
+      name:'Steve',
+      birthYear: '1969',
+      deathYear: 'Still alive',
+      bio:'Great guy'
+    }
+  }
+];
+//Responds by taking you to index
+app.get('/',(req,res) =>{
+  res.send('Welcome to FlixFans!!');
+});
 
-  
-  // GET requests
-  app.get('/', (req, res) => {
-    let responseText= 'Welcome to FlixFans!!';
-    responseText += '<small>Request at: ' + req.requestTime + '</small>';
-    
-    res.send(responseText);
-  });
+//responds with list of all movies
+app.get('/movies',(req,res)=>{
+  res.json(movies);
+});
 
-  //useing express function to get all satic files in pulic folder
-  app.use(express.static('public'));
-  
-  app.get('/topMovies', (req, res) => {
-    res.json(topMovies);
-  });
+//Responds with a specific movie by title
+app.get('/movies/:title',(req,res) =>{
+  res.json(movies.find((movie) => {
+    return movie.title === req.params.title
+  }));
+});
 
+//Respond with all movies in specified genre
+app.get('/movies/genres/:genre', (req, res) => {
+  const genre = movies.find((movie) => movie.genres.type === req.params.genre).genre;
 
-  //error handler  err.stack console logs error
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
+  if(genre) {
+    res.status(200).json(genre);
+  }else{
+    res.status(404).send('Genre not found!');
+  }
+});
 
-  // listen for requests on 8080
-  const port = 5000;
-  app.listen(port, () => {
-    console.log('Your app is listening on port 5000.');
-  });
+//Responds with json data about specified director
+app.get('movies/director/:name', (req,res) => {
+  const director = movies.find((movie) => {return movie.director.name === req.params.name});
+
+  if(director){
+    res.status(200).json(director);
+  }else{
+    res.status(404).send('Director not found!');
+  }
+});
+
+//Create a new user using UUID
+app.post('/users',(req,res) => {
+  const newUser = req.body;
+
+  if(newUser.username){
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(200).json(newUser);
+  }else{
+    const message = ('Missing username in request body');
+    res.status(400).send(message);
+  }
+});
+
+//Update existing username
+app.put('users/:username',(req,res) =>{
+  let user = users.find((user) => {return user.username === req.params.username});
+
+  if(user) {
+    user.username = newUsername.username;
+    res.status(200).json(user)
+  }else{
+    res.status(404).send('User not found')
+  };
+});
+
+//Add movie to users favorites
+app.post('users/:username/:movie', (req,res) =>{
+  let user = users.find((user) => {return user.usersname = req.params.username});
+
+  if (user){
+    user.favorites.push(req.params.movie);
+    res.status(200).send(req.params.movie + ' has been added to ' + user.username + "'s list of favorites!");
+  }else{
+    res.status(404).send('User or movie not found');
+  };
+});
+
+//Remove movie from users favorites
+app.delete('/users/:username/:movie',(req,res) =>{
+  let user = users.find((user) => {return user.username === req.params.username});
+
+  if (user) {
+     user.favorites = user.favorites.filter((movie) => {return movie !== req.params.movie});
+     res.status(200).send(req.params.movie + ' was removed from your favorites list!');
+  }else{
+    res.status(404).send('User was not found');
+  }
+});
+
+//using express to search public folder for any "/" extension that doesnt have an endpoint specified
+app.use(express.static('public'));
+
+//error handler
+app.use((err,req,res,next) => {
+  console.error(err.stack);
+  res.status(400).send('Something went wrong!')
+});
+//telling which port to listen to for requests
+app.listen(8080,() =>{
+  console.log('Your app is listening on 8080');
+});
