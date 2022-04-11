e92ab014555b2dd8342ec4afbed9b2aac29abd0e
smo92 <moran.s215@gmail.com>
2022-04-11 15:24:01 -0600

    Imported mongoDB collections to Atlas and then changed mongoose.connect to the proper URI saved on Heroku

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/FlixFansDB',
+mongoose.connect(process.env.CONNECTION_URI,
   {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
